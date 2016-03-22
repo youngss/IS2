5d51abc3f30dac6e2844b9c4efdccb49052b3126
youngss <User@172.31.45.30>
2016-03-22 13:17:24 +0200

    changee

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,8 +2,8 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hello Class");
+		System.out.println("Hello sec2 cs381");
+		
 	}
 
 }
